9f663bac78cd5a6ec3c1067a9545c5ad99c935a7
MichelAihara <michel_aihara@yahoo.com.br>
2019-01-09 12:13:57 -0200

    Adicionei arquivos via upload

---
+++ Hello GitHub/src/br/com/michelaihara/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.michelaihara.git;
+
+/**
+ * @author suporte
+ *Integração do eclips com github
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git, testando integração");
+
+	}
+
+}
