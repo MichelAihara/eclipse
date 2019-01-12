8d3addf7a3cffa79cfc7a4df3ce4296bde1a5d4b
MichelAihara <michel_aihara@yahoo.com.br>
2019-01-12 11:40:29 -0200

    Add nova classe

--- Hello GitHub/src/br/com/michelaihara/git/HelloGit.java
+++ Hello GitHub/src/br/com/michelaihara/git/HelloGit.java
@@ -6,18 +6,15 @@ package br.com.michelaihara.git;
 import javax.swing.plaf.synth.SynthSeparatorUI;
 
 /**
- * @author suporte
  *Integração do eclips com github
  */
 public class HelloGit {
 
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
 		System.out.println("Hello Git, testando integração");
 		System.out.println("Add segunda linha");
-		System.out.println("Upload em dia diferente, testando conexão com GIT");
+		System.out.println("Upload em dia diferente, testando conexão com Git");
 
 	}
 
---
+++ Hello GitHub/src/br/com/michelaihara/git/funcionario.java
@@ -0,0 +1,8 @@
+package br.com.michelaihara.git;
+
+public class funcionario {
+	
+	public String nome;
+	public String matricula;
+
+}
